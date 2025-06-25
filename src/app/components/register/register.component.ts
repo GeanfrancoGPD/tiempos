e54ca314e6d6d46cb782129d07952784f34e1ba3
Geanfranco <geanfranco1212@gmail.com>
2025-06-24 21:46:25 -0400

    feat:Se agrego el registro

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,21 +2,29 @@ import { Component } from '@angular/core';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { Auth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  email = '';
+  gmail = '';
   password = '';
   error = '';
 
   constructor(private auth: Auth, private router: Router) {}
 
   register(){
-      createUserWithEmailAndPassword(this.auth)
+      createUserWithEmailAndPassword(this.auth, this.gmail, this.password)
+      .then(() => {
+      this.router.navigate(['/']);
+    })
+    .catch(err => {
+      this.error = err.message;
+    });
   }
 }
